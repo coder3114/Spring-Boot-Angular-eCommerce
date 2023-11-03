dd2d866de2e9525bdf4721b8e3cd18a976710108
coderella <qiangie.yang@gmail.com>
2023-11-03 17:06:44 +0000

    Set up backend

---
+++ backend/src/main/java/com/enqidev/ecommerce/entity/Product.java
@@ -0,0 +1,70 @@
+package com.enqidev.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "ingredients")
+    private List<String> ingredients;
+
+    @Column(name = "preparation_time")
+    private int preparationTime; // The time required to prepare the dish (in minutes)
+
+    @Column(name = "is_vegetarian")
+    private boolean isVegetarian;
+
+    @Column(name = "is_vegan")
+    private boolean isVegan;
+
+    @Column(name = "is_gluten_free")
+    private boolean isGlutenFree;
+
+    @Column(name = "is_dairy_free")
+    private boolean isDairyFree;
+
+    @Column(name = "is_nut_free")
+    private boolean isNutFree;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+//    @Column(name = "date_updated")
+//    @UpdateTimestamp
+//    private Date lastUpdated;
+
+}
---
+++ backend/src/test/java/com/enqidev/ecommerce/SpringBootAngularEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enqidev.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
