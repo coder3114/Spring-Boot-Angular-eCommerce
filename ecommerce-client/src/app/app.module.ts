f798e4e6f4902e940c2356c11fc9ca5e44cb803e
coderella <qiangie.yang@gmail.com>
2023-11-06 20:47:48 +0000

    Implement angular frontend - sidebar & header

--- ecommerce-client/src/app/app.module.ts
+++ ecommerce-client/src/app/app.module.ts
@@ -3,12 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf, NgFor } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgFor,
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
